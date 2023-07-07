a7a31dd4ac5aa48db88859bdf0be366111df5ee5
madina_egorova <madina_egorova_04@mail.ru>
2023-07-07 17:04:35 +0300

    Initial commit

---
+++ app2/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:app2/activities/home.dart';
+void main() => runApp(MaterialApp(
+  theme: ThemeData(
+    primaryColor: Colors.deepOrangeAccent,
+  ),
+  home: Home(),
+));
+
+
+
+
+
